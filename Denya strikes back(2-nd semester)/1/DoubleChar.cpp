16e57802a5cf4345681a6655e96ea1e2d475cb26
klesun <arturklesun@gmail.com>
2016-02-20 14:21:06 +0200

    Изменения:Раньше программа читала только первое слово в строке, сейчас же она читает все слова в одной строке.

--- Denya strikes back(2-nd semester)/1/DoubleChar.cpp
+++ Denya strikes back(2-nd semester)/1/DoubleChar.cpp
@@ -1,24 +1,37 @@
 #include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+
 using namespace std;
 
 const int MAX_STRING_LENGTH = 100;
 char* doubleChar(char * source);
+void doOneString();
 
 int main()
 {
-    char input[MAX_STRING_LENGTH];
-    cout << "Please enter your string\n";
-    cin >> input;
-    
-    char* result = doubleChar(input);
-    cout << "your result is=" << result << endl;
-    
-    free(result);
+    doOneString();
+    // doStringArray();
+
     return 0;
 }
-/* Например у нас есть слово "хуй" , если мы удвоим каждый 
+
+void doOneString()
+{
+    char input[MAX_STRING_LENGTH];
+
+    cout << "Please enter your string\n";
+    // http://stackoverflow.com/a/318799/2750743
+    scanf("%[^\n]", input);
+
+    char* result = doubleChar(input);
+    cout << "your result is=" << result << endl;
+
+    free(result);
+}
+/* Например у нас есть слово "хуй" , если мы удвоим каждый
 символ этой строки, то у нас получиться "ххууйй"*/
-char* doubleChar(char * source) 
+char* doubleChar(char * source)
 {
     char* result = (char*)malloc(MAX_STRING_LENGTH * 2);
     for(int i=0; source[i] != 0; ++i) {
